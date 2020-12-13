470126fce19e11caafef3d35aa320536dd71744a
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-13 09:22:28 -0300

    feat(peer-signaling): adicionado biblioteca de sinalização

--- libs/peer/core/src/lib/peer-event.enum.ts
+++ libs/peer/core/src/lib/peer-event.enum.ts
@@ -1,7 +1,8 @@
 export enum PeerEvent {
   Disconnected = 'peer-disconnected',
   Connected = 'peer-connected',
-  ConnectToRoom = 'connect-to-room',
+  JoinToRoom = 'join-to-room',
+  JoinedToRoom = 'joined-to-room',
   ScreenShared = 'screen-shared',
   Message = 'peer-message',
   SdpAnswer = 'sdp-answer',
--- libs/peer/signaling/src/index.ts
+++ libs/peer/signaling/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/peer-signaling.module'
+export * from './lib/peer-signaling.logger'
+export * from './lib/peer-signaling.gateway'
