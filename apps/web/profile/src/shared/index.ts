130baf35f78356420289be0c16153bdb6b34d130
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-04-29 07:36:39 -0300

    feat(web-profile): drop-down navigation

--- apps/web/profile/src/shared/index.ts
+++ apps/web/profile/src/shared/index.ts
@@ -1,5 +1,6 @@
 export * from './code-editor/code-editor.element'
-export * from './front-end/front-end.element'
 export * from './txt-rotate/txt-rotate.element'
+export * from './front-end/front-end.element'
+export * from './drop-down/drop-down.element'
 export * from './parallax-effect'
 export * from './dom-helpers'
