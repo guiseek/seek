640cb4246e17d52ee1971b0179fb67328a9f52b6
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-04-30 18:41:55 -0300

    fix(web-transfer): route

--- apps/web/transfer/src/app/app.element.ts
+++ apps/web/transfer/src/app/app.element.ts
@@ -1,15 +1,33 @@
+import { WebElementConfig } from './core/element'
+import { WebPage, routes } from './core/page'
 import { html } from '@guiseek/web-core'
 import './app.element.scss'
 
-export class AppElement extends HTMLElement {
-  public static observedAttributes = []
+@WebElementConfig('app-page')
+export class AppElement extends WebPage {
+  constructor() {
+    super(html`<main id="outlet"></main>`)
 
+    const route = location.pathname
+
+    if (routes.get(route)) {
+      const tag = routes.get(route)
+      const page = document.createElement(tag)
+
+      const outlet = this.query('#outlet')
+      if (outlet) outlet.appendChild(page)
+    }
+  }
   connectedCallback() {
-    this.innerHTML = html`
-      <main>
-        <transfer-page></transfer-page>
-      </main>
-    `
+    const links = this.queryAll('ul.menu-bar li')
+    if (links.length) this.toggleLinks(links)
+  }
+
+  toggleLinks(links: NodeListOf<HTMLElement>) {
+    links.forEach((link) => {
+      const classList = document.body.classList
+      link.onmouseover = () => classList.toggle('invert')
+      link.onmouseout = () => classList.toggle('invert')
+    })
   }
 }
-customElements.define('seek-peer-root', AppElement)
